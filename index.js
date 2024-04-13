176087a97d49c90ab6912ecfaecbd4824113c687
Your Name <you@example.com>
2024-04-13 16:38:10 +0530

    g

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const { Mongoose } = require("mongoose");
 const app = express();
 const mongoose = require("mongoose");
 require('dotenv').config();
+const cors = require("cors");
 
 mongoose.connect(process.env.URL);
+app.use(cors(process.env.CORSURL))
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
